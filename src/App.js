12e74e5b8510c800d6a3c431a19a6597232b3ad1
Dom Vu <Dominicvu36@gmail.com>
2020-04-18 22:07:26 -0600

    fix app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import MovieHeader from './components/movieheader';
+import MovieList from './components/movielist';
+import Movie from './components/movie';
 import Authentication from './components/authentication';
 import {HashRouter,Route} from 'react-router-dom';
 import { Provider } from 'react-redux'
@@ -16,6 +18,9 @@ class App extends Component {
                         <MovieHeader />
                         <Route exact path="/" render={()=><div />}/>
                         <Route path="/signin" render={()=><Authentication />}/>
+						<Route exact path="/" render={()=><MovieList />}/>
+                        <Route path="/movielist" render={()=><MovieList />}/>
+                        <Route path="/movie/:movieId" render={()=><Movie />}/>
                     </div>
                 </HashRouter>
                 </Provider>
