5b08e38766169817271d9464b0726b71d45955d9
Dom Vu <Dominicvu36@gmail.com>
2020-04-18 22:12:26 -0600

    fix app

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from "../reducers/authReducer";
+import movieReducer from "../reducers/movieReducer";
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
@@ -12,6 +13,7 @@ if (process.env.NODE_ENV === 'development') {
 const store = createStore(
     combineReducers( {
         auth: authReducer
+		movie: movieReducer
     }),
     applyMiddleware(
         ...middlewares
